50ee1d01ed4a4d45071f5131d6e94c94a5e2eca3
AA <angelangelov97@gmail.com>
2020-09-07 04:14:42 +0300

    fix logo icon

--- src/components/Token.vue
+++ src/components/Token.vue
@@ -1,9 +1,9 @@
 <template>
   <img
     :src="
-      `https://raw.githubusercontent.com/balancer-labs/snapshot/develop/src/assets/${namespace}.png`
+      `https://curve.fi/logo.png`
     "
-    class="d-inline-block bg-gray-9 v-align-middle line-height-0 circle border"
+    class="d-inline-block v-align-middle line-height-0"
     :style="{
       width: `${size || 22}px`,
       height: `${size || 22}px`
